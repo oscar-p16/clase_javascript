69cbf672ba578bfbfd25801b1dc7ad6e91d025b9
oscar p <oscarperezm16@hotmail.com>
2021-05-01 17:06:34 -0500

    comentario, sentencias, variables,constantes y uso del let

---
+++ script.js
@@ -0,0 +1,32 @@
+alert('soy javascript');
+
+//comentario de linea
+/*comentario
+mas de una linea*/
+
+//sentencias
+//alert('hola'); alert('mundo');
+
+//variables
+let mensaje;
+mensaje='hola'
+alert(mensaje)
+
+mensaje= 'hola mundo 2'
+alert(mensaje)
+
+let a=6;
+let b=5;
+let suma= a+b;
+console.log('el resultado de la suma es:',suma);
+
+//constante
+const PI= 3.141624;
+const COLOR_ROJO='#FFFF';
+pi_dos=PI;
+pi_dos=7;
+console.log(pi_dos);
+console.log(PI);
+
+
+
