d3fead96940f4aacc1ee2db45e615ce2780bb777
oscar p <oscarperezm16@hotmail.com>
2021-05-01 20:35:17 -0500

    tipos de datos y el primer error de javascript

--- script.js
+++ script.js
@@ -28,5 +28,70 @@ pi_dos=7;
 console.log(pi_dos);
 console.log(PI);
 
+//tipos de datos
+let mensaje_tres= 'hola';
+mensaje_tres= 1234; 
+
+//numero
+let n = 123;
+n= 12.456;
+
+//infinity
+console.log(1/0);
+console.log(Infinity);
+
+//NaN error de calculo
+console.log('no es un numero'/2);
+
+//NaN es pegajoso
+console.log('no es un numero'/2 + 5);
+
+//bigint valores mayores y menores de +/-(2^53-1) 
+let numero_grande= 12345667889474747477347373473473734943949;
+console.log(numero_grande)
+
+//tipo String
+let t = b;
+let texto = "hola";
+let texto_dos = 'hola';
+let texto_tres = ``; // backticks funcionalidad extendida
+let nomb= 'oscar';
+console.log('hola',nomb);
+console.log('hola', nomb);
+console.log("hola ${nomb}");
+console.log('hola ${nomb}');
+console.log(`hola ${nomb}`);
+
+//tipo booleano
+//true y false
+let verdadero = true;
+let falso = false;
+
+let mayor = 4 > 1; //true
+
+//tipo de dato null
+//es un tipo propio que solo contiene null
+let edad = null;
+console.log(edad);
+//no es un objeto no existente
+//es solo un valor especial que representa nada vacio.
+
+//valor undefined
+//valor especial que no pertenece a ningun tipo de dato anterior.
+//significado valor no asignado
+let edad_dos;
+console.log(edad_dos);
+
+//el operador typeof o funcion typeoj(X), decuelve el tipo de dato
+console.log(typeof undefined);
+console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof 10n);
+console.log(typeof 'hola');
+
+console.log(typeof(alert));
+console.log(typeof(null));
+
+
 
 
